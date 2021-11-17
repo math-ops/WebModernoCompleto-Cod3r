adf6fa419e76cc0fae8c42f192a2de6d061b46d4
Matheus Oliveira <matheusops.pc@gmail.com>
2021-11-17 15:03:15 -0400

    aulas sessão 04

---
+++ Seções/Seção 4 - Javascript (Fundamentos)/usandoVar1.js
@@ -0,0 +1,20 @@
+{
+  {
+    {
+      {
+        var teste = 'teste?'
+        console.log(teste);
+      }
+    }
+  }
+}
+
+console.log(teste);
+
+function test(){
+  var local = 123;
+  console.log(local);
+}
+
+test()
+console.log(local);
